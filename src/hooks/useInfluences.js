137e0428bad7530b6f3c5ecafd4678d93a90409e
David <sanderdj90@gmail.com>
2021-04-27 14:02:28 -0600

    feat: useInfluences

---
+++ src/hooks/useInfluences.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+export const useInfluences = () => {
+  const data = useStaticQuery(graphql`
+    query InfluenceQuery {
+      influences: allAirtable(filter: { table: { eq: "Influence" } }) {
+        totalCount
+        nodes {
+          table
+          recordId
+          data {
+            Name
+            Notes
+            Description
+            Artist
+          }
+        }
+      }
+    }
+  `)
+  const influences = data?.influences?.nodes
+  return {
+    influences,
+  }
+}
