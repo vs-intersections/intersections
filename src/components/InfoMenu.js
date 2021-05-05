4765d3e46a5e68ecb2d9f5811461606c586d77f0
David J. Sanderson <sanderdj90@gmail.com>
2021-05-04 21:43:08 -0600

    feat(InfoMenu): now expandable

--- src/components/InfoMenu.js
+++ src/components/InfoMenu.js
@@ -1,10 +1,18 @@
-import React from "react"
+/** @jsx jsx */
+import React, { useState } from "react"
+import { css, jsx } from "@emotion/react"
 import { BsInfoCircleFill } from "react-icons/bs"
-
 const InfoMenu = () => {
+  const [infoMenuIsOpen, setInfoMenuIsOpen] = useState(false)
   return (
-    <div className="flex py-4 justify-center items-center bg-gray-200">
-      <BsInfoCircleFill className="w-6 h-6" />
+    <div
+      className="bg-gray-200 h-screen z-50 flex justify-center transition-transform"
+      css={css`
+      height: ${infoMenuIsOpen ? "calc(100vh - 64px)" : "100%"};
+      transform: ${infoMenuIsOpen ? "translateY(-100%)" : ""};
+      `}
+    >
+      <BsInfoCircleFill onClick={() => setInfoMenuIsOpen(!infoMenuIsOpen)} className="w-6 h-6 cursor-pointer mt-3" />
     </div>
   )
 }
