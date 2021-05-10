78958fb0332f05d202835bbe60870946b15be913
imbrikis <imbrikis@yahoo.com>
2021-05-10 10:44:29 -0600

    styling about and video pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center bg-gray-100 w-100vw">
+    <div className="flex items-center justify-center bg-gray-100 w-full">
       &copy;{new Date().getFullYear()} Vital Spaces - All Rights Reserved
     </div>
   )
