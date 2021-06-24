5a23f790fb8ad909fb1ed14e3d8919caeadd70f7
imbrikis <imbrikis@yahoo.com>
2021-06-24 08:57:56 -0600

    content: update about page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
 import React from "react"
+import { useWindowSize } from "../hooks"
 
 const Footer = () => {
+  const { width } = useWindowSize()
+  const IS_MOBILE = width <= 1024
+
   return (
-    <div className="fixed bottom-0 h-8 flex items-center justify-center bg-gray-100 w-screen">
+    <div
+      className={`fixed bottom-0 h-8 flex items-center justify-center bg-gray-100 w-screen ${
+        IS_MOBILE ? "text-sm" : ""
+      }`}
+    >
       &copy;{new Date().getFullYear()} Vital Spaces - All Rights Reserved
     </div>
   )
